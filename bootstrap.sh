9577e028ad5843201139d13eb533f3dd9df5fb8a
Samuel Jahnke <asamwow@github.com>
2023-08-30 09:22:14 -0500

    prompt rename branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
-git init
-git remote add origin https://github.com/asamwow/CastlesMods.git
-git fetch
-git reset --soft origin/main
-git add .
+git init &&
+git remote add origin https://github.com/asamwow/CastlesMods.git &&
+git fetch &&
+git reset --soft origin/main &&
+git add . &&
+read -p "Enter Branch Name: " branchName &&
+git branch -m $branchName &&
 git status
