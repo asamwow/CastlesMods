33e96cf3e8eb56caa01c4517699eeff027026e17
Samuel Jahnke <asamwow@github.com>
2023-09-01 07:57:16 -0500

    forgot to tangle

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,4 @@ git remote add origin https://github.com/asamwow/CastlesMods.git &&
 git fetch &&
 git reset --soft origin/main &&
 git add . &&
-read -p "Enter Branch Name: " branchName &&
-git branch -m $branchName &&
 git status
